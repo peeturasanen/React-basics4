806ca565749ebec5448ada0fd313af85e25bab40
Peetu <peetu.rasanen@metropolia.fi>
2024-01-22 14:02:46 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import WelcomeMessage from './WelcomeMessage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <WelcomeMessage name="Alice" message="This is a welcome message">
+                <p>This is some additional JSX passed as children.</p>
+            </WelcomeMessage>
+        </div>
+    );
 }
 
 export default App;
